93ee3a96b56b908347202f12d0e5f697162d0979
senerio <skarlsefni@gmail.com>
2024-02-19 01:28:16 +0800

    scratchcard return link and select card

---
+++ scratchcard.js
@@ -0,0 +1,18 @@
+// ==UserScript==
+// @name         Neopets - Scratchcards
+// @version      2024-02-18
+// @description  Adds link back to kiosk, auto selects card from the dropdown
+// @author       senerio
+// @match        *://*.neopets.com/winter/kiosk*.phtml*
+// @match        *://*.neopets.com/halloween/scratch*.phtml*
+// @icon         https://www.google.com/s2/favicons?sz=64&domain=neopets.com
+// @grant        none
+// ==/UserScript==
+
+const path = window.location.pathname;
+if(path.includes('2')) {
+    $('.content form input').before(`<p><a href="${path.replace('2','')}.phtml">Scratch again</a></p>`);
+}
+else {
+    $('select[name=card_id] option').eq(1).prop('selected', true);
+}
