2b7d8fff1c08ac1c46396ee5864e40a0eef0563c
senerio <skarlsefni@gmail.com>
2024-03-11 20:10:54 +0800

    Update obelisk.js - add boon details

--- obelisk.js
+++ obelisk.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Neopets - Obelisk
-// @version      2024-03-06
-// @description  Handle getting stuck after joining an Obelisk faction
+// @version      2024-03-11
+// @description  Handle getting stuck after picking a faction/boon; displays boon details
 // @author       senerio
 // @match        *://*.neopets.com/prehistoric/battleground/
 // @icon         https://www.google.com/s2/favicons?sz=64&domain=neopets.com
@@ -9,7 +9,37 @@
 // @run-at       document-idle
 // ==/UserScript==
 
-const obeliskScript = document.querySelector('#pageDesc + style + script');
-const newObeliskScript = obeliskScript.innerHTML.replace('_gaq.push(["_trackEvent", "battleground", "joined-faction", faction]);', '')
-obeliskScript.remove();
-document.body.appendChild(Object.assign(document.createElement("script"), { innerHTML: newObeliskScript }));
+// Stuck fix
+const obeliskScript = document.querySelector('#pageDesc + style + script') || document.querySelector('#pageDesc + script');
+if(obeliskScript) {
+    const newObeliskScript = obeliskScript.innerHTML.replace(/_gaq.push.*;/g, '')
+    obeliskScript.remove();
+    document.body.appendChild(Object.assign(document.createElement("script"), { innerHTML: newObeliskScript }));
+}
+
+// Boon details
+const boons = {
+    "bankbribery": "bank interest +3%",
+    "blackmarket": "highlight exclusive items",
+    "booksmarts": "+2-4 INT after reading",
+    "cartogriphication": "Faerie Caverns direction",
+    "cheaperdozen": "10NP Stock Market minimum",
+    "doctorwho?": "cures sick neopet in the springs",
+    "doppelganger": "chance to reuse one-use BD items",
+    "doublebubble": "chance of potion refill",
+    "equipall": "+1  weapon slot",
+    "fivefinger": "10% neopian shop discount",
+    "grrraaaahhhhhh": "BD damage increase by 10%",
+    "lolavies": "steal someone's avatar",
+    "refreshquest": "one refresh per faerie quest",
+    "rightround": "+1 wheel spin",
+    "scratchmaster": "+1 scratchcard purchase",
+    "strengthmind": "Mind Blast BD ability (based on INT)",
+    "millionairefeeling": "TP offer limit up to 2m (useless)",
+    "fullpockets": "+NP from premium scratch card",
+    "premiumdream": "+1 send score on featured game"
+}
+$('.perks li div').each((i,e) => {
+    const name = e.style['background-image'].match('obelisk/(.*)_')[1];
+    $(e).parent().append(`<p class="sf">${boons[name]}</p>`);
+})
