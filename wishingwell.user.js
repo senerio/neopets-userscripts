ea9cb3813fb55210c3475e219032cfd4dd0cc591
senerio <skarlsefni@gmail.com>
2024-05-15 23:15:43 +0800

    Update wishingwell.user.js - added Monfo's stop autofill if 7 wishes are done

--- wishingwell.user.js
+++ wishingwell.user.js
@@ -1,12 +1,17 @@
 // ==UserScript==
 // @name         Neopets - Wishing Well autofill
-// @version      2024-02-12
-// @description  Autofill donation amount and item name
-// @author       senerio
+// @version      2024-05-15
+// @description  Autofill donation amount and item name; no autofill if 7 wishes are done
+// @author       senerio, Monfo
 // @match        *://*.neopets.com/wishing.phtml*
 // @icon         https://www.google.com/s2/favicons?sz=64&domain=neopets.com
 // @grant        none
 // ==/UserScript==
 
-$('input[name=donation]').val('21');
-$('input[name=wish]').val('Neopets 24th Birthday Goodie Bag');
+// Change the "item" variable to whichever item you'd like to wish for
+const item = 'Snowager Stamp';
+
+if ((!window.location.search && document.querySelector('.content').children[9].children.length === 1) || (window.location.search && document.querySelector('.content').children.length === 14)) {
+    $('input[name=donation]').val('21');
+    $('input[name=wish]').val(item);
+};
